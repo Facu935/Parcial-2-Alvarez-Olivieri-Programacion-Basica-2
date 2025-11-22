314bf18489bb2958b850262b77fb0a0178209828
Felipe <n.felialvarez@gmail.com>
2025-11-22 16:36:50 -0300

    se agregaron nuevos test para Transformaciones elementales, ademas se agrego @Before para mejor legibilidad del codigo

--- test/ar/edu/unlam/pb2/criaturas/TransformacionesElementalesTest.java
+++ test/ar/edu/unlam/pb2/criaturas/TransformacionesElementalesTest.java
@@ -2,78 +2,118 @@ package ar.edu.unlam.pb2.criaturas;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TransformacionesElementalesTest {
-
+	private ICriatura miCriatura1;
+	private ICriatura miCriatura2;
+	private ICriatura miCriaturaAgua;
+	private ICriatura criaturaConBajaEnergia;
+	
+	@Before
+	public void setUp() throws Exception {
+		miCriatura1 = new CriaturaSalvaje("Marcos", 50, Afinidades.FUEGO, ComportamientoEmocional.TRANQUILA);
+		miCriatura2 = new CriaturaSalvaje("Marcos", 100, Afinidades.FUEGO, ComportamientoEmocional.TRANQUILA);
+		miCriaturaAgua = new CriaturaSalvaje("Marcos", 100, Afinidades.AGUA, ComportamientoEmocional.TRANQUILA);
+		criaturaConBajaEnergia = new CriaturaSalvaje("Felipe", 5, Afinidades.AGUA, ComportamientoEmocional.TRANQUILA);
+	}
+	
 	@Test
 	public void validarQueAlAplicarBendicionDelRioLaEnergiaSeDuplique() {
-		ICriatura miCriatura = new CriaturaSalvaje("Marcos", 50, Afinidades.FUEGO, ComportamientoEmocional.TRANQUILA);
-
-		Integer energiaInicial = miCriatura.getNivel();
+		Integer energiaInicial = miCriatura1.getNivel();
 		Integer energiaInicialEsperada = 50;
 		assertEquals(energiaInicialEsperada, energiaInicial);
 
-		miCriatura = new BendicionDelRio(miCriatura);
+		miCriatura1 = new BendicionDelRio(miCriatura1);
 		
-		Integer energiaActual = miCriatura.getNivel();
+		Integer energiaActual = miCriatura1.getNivel();
 		Integer energiaEsperada = 100;
 		assertEquals(energiaEsperada, energiaActual);
 	}
 	
-	// TEST PARA HACER
-	// - Validar que al duplicarse no supere el limite de energia
+	@Test
+	public void validarQueAlAplicarBendicionDelRioLaEnergiaNoSupereElLimite() {
+		Integer energiaInicial = miCriatura2.getNivel();
+		Integer energiaInicialEsperada = 100;
+		assertEquals(energiaInicialEsperada, energiaInicial);
+
+		miCriatura2 = new BendicionDelRio(miCriatura2);
+		
+		Integer energiaActual = miCriatura2.getNivel();
+		Integer energiaEsperada = 180;
+		assertEquals(energiaEsperada, energiaActual);
+	}
 
 	@Test
 	public void validarQueAlAplicarLlamaInternaAUnaCriaturaDeFuegoTengaMasTreintaDeEnergia() {
-		ICriatura miCriatura = new CriaturaSalvaje("Polo", 50, Afinidades.FUEGO, ComportamientoEmocional.TRANQUILA);
-
-		Integer energiaInicial = miCriatura.getNivel();
+		Integer energiaInicial = miCriatura1.getNivel();
 		Integer energiaInicialEsperada = 50;
 		assertEquals(energiaInicialEsperada, energiaInicial);
 
-		miCriatura = new LlamaInterna(miCriatura);
+		miCriatura1 = new LlamaInterna(miCriatura1);
 		
-		Integer energiaActual = miCriatura.getNivel();
+		Integer energiaActual = miCriatura1.getNivel();
 		Integer energiaEsperada = 80;
 		assertEquals(energiaEsperada, energiaActual);
 	}
 	
 	@Test
 	public void validarQueAlAplicarLlamaInternaAUnaCriaturaDistintaDeFuegoSeVuelvaInestable() {
-		ICriatura miCriatura = new CriaturaSalvaje("Polo", 50, Afinidades.AGUA, ComportamientoEmocional.TRANQUILA);
-
-		ComportamientoEmocional comportamientoInicial = miCriatura.getComportamiento();
+		ComportamientoEmocional comportamientoInicial = miCriaturaAgua.getComportamiento();
 		ComportamientoEmocional comportamientoInicialEsperada = ComportamientoEmocional.TRANQUILA;
 		assertEquals(comportamientoInicialEsperada, comportamientoInicial);
 
-		miCriatura = new LlamaInterna(miCriatura);
+		miCriaturaAgua = new LlamaInterna(miCriaturaAgua);
 		
-		ComportamientoEmocional comportamientoActual = miCriatura.getComportamiento();
+		ComportamientoEmocional comportamientoActual = miCriaturaAgua.getComportamiento();
 		ComportamientoEmocional comportamientoEsperado = ComportamientoEmocional.INESTABLE;
 		assertEquals(comportamientoEsperado, comportamientoActual);
 	}
 	
-	// Puede haber un test de: Si la criatura no es fuego pero su ComportamientoEmocional ya es INESTABLE, puede haber una EXCEPCION??
+	@Test
+    public void validarQueAlAplicarLlamaInternaAUnaCriaturaDeAguaLaEnergiaNOCambie() {
+        Integer energiaInicial = miCriaturaAgua.getNivel();
+        
+        miCriaturaAgua = new LlamaInterna(miCriaturaAgua);
+     
+        Integer energiaActual = miCriaturaAgua.getNivel();
+        
+        assertEquals(energiaInicial, energiaActual);
+    }
+	
+	@Test
+    public void validarQueAlAplicarLlamaInternaAUnaCriaturaDeFuegoMantengaSuComportamiento() {
+        assertEquals(Afinidades.FUEGO, miCriatura1.getAfinidad());
+        assertEquals(ComportamientoEmocional.TRANQUILA, miCriatura1.getComportamiento());
+
+        miCriatura1 = new LlamaInterna(miCriatura1);
+
+        assertEquals(ComportamientoEmocional.TRANQUILA, miCriatura1.getComportamiento());
+    }
 	
 	@Test
 	public void validarQueAlAplicarVinculoTerrestreAUnaCriaturaSuEnergiaNoQuedeMenorACincuenta() {
-		ICriatura miCriatura = new CriaturaSalvaje("Polo", 10, Afinidades.AGUA, ComportamientoEmocional.TRANQUILA);
-
-		Integer energiaInicial = miCriatura.getNivel();
-		Integer energiaInicialEsperada = 10;
+		Integer energiaInicial = criaturaConBajaEnergia.getNivel();
+		Integer energiaInicialEsperada = 5;
 		assertEquals(energiaInicialEsperada, energiaInicial);
 
-		miCriatura = new VinculoTerrestre(miCriatura);
+		criaturaConBajaEnergia = new VinculoTerrestre(miCriatura1);
 		
-		Integer energiaActual = miCriatura.getNivel();
+		Integer energiaActual = criaturaConBajaEnergia.getNivel();
 		Integer energiaEsperada = 50;
 		assertEquals(energiaEsperada, energiaActual);
 	}
 	
-	
-	// Validar que al aplicar Vinculo Terrestre a una criatura con +50 energia retorne su energia original
-	// Oportunidad de mejora: Test GENERAL que valide la parte de energiaInicial/energiaEsperada?
+	@Test
+    public void validarQueAlAplicarVinculoTerrestreConEnergiaAltaNoSeModifique() {
+        Integer energiaInicialEsperada = 100;
+        assertEquals(energiaInicialEsperada, miCriatura2.getNivel());
+
+        miCriatura2 = new VinculoTerrestre(miCriatura2);
+        
+        assertEquals(energiaInicialEsperada, miCriatura2.getNivel());
+    }
 	
 	@Test
 	public void validarQueAlAplicarAscensoDelVientoSuAfinidadSeaAire() {
@@ -88,5 +128,14 @@ public class TransformacionesElementalesTest {
 		Afinidades afinidadActual = miCriatura.getAfinidad();
 		Afinidades afinidadEsperada = Afinidades.AIRE;
 		assertEquals(afinidadEsperada, afinidadActual);
+	}
+	
+	@Test
+	public void validarQueSePuedanCombinarDosTransformaciones() {
+	    miCriatura1 = new LlamaInterna(miCriatura1);
+	    miCriatura1 = new BendicionDelRio(miCriatura1);
+	    
+	    Integer energiaEsperada = 160; 
+	    assertEquals(energiaEsperada, miCriatura1.getNivel());
 	}
 }
